19620c49887eb80f05b50ef6942920335a466ae7
Chris Zimmerman <chris-zimmerman@live.com>
2019-12-23 10:59:17 -0500

    Tensor printing works

---
+++ README.md
@@ -0,0 +1,18 @@
+Vnum is in active development, and contributions are always welcome!
+
+```v
+fn main() {
+	t := from_shape([2, 2, 2])
+	t.set([0, 1, 1], 2.0)
+	t.set([1, 0, 0], 3.14)
+	println(t)
+}
+```
+
+```v
+Tensor([[[0.000000, 0.000000],
+         [0.000000, 2.000000]],
+
+        [[3.140000, 0.000000],
+         [0.000000, 0.000000]]]
+```
--- src/tensor.v
+++ src/tensor.v
@@ -1,3 +1,5 @@
+import strings
+
 struct Tensor {
 	shape []int
 	strides []int
@@ -106,7 +108,7 @@ fn (t Tensor) flat_iter() NdIter {
 }
 
 fn (iter mut NdIter) next() &f64 {
-	mut ret := iter.ptr
+	ret := iter.ptr
 	mut i := iter.dim
 	for i >= 0 {
 		iter.track[i] += 1
@@ -115,13 +117,121 @@ fn (iter mut NdIter) next() &f64 {
 
 		if (iter.track[i] == iter.shape[i]) {
 			iter.track[i] = 0
-			ret -= (shape_i - 1) * stride_i
+			iter.ptr -= (shape_i - 1) * stride_i
 			i--
 			continue
 		}
-		ret += stride_i
+		iter.ptr += stride_i
 		break
 	}
-	iter.ptr = ret
 	return ret
+}
+
+struct Printer {
+	t Tensor
+	idx []int
+	ptr &f64
+	iter NdIter
+	obrackets string
+	cbrackets string
+	indent string
+	last_comma int
+	yielded int
+	mut:
+		strides []int
+		shape []int
+		io strings.Builder
+}
+
+fn init_printer(tns Tensor) Printer {
+	ob := "[".repeat(tns.ndims)
+	cb := "]".repeat(tns.ndims)
+	id := " ".repeat(tns.ndims)
+	mut prnt := Printer{
+		t: tns
+		iter: tns.flat_iter()
+		idx: [0].repeat(tns.ndims)
+		ptr: tns.buffer
+		obrackets: "Tensor" + "(" + ob
+		cbrackets: cb
+		indent: id
+		last_comma: 0
+		strides: tns.strides
+		shape: tns.shape
+		io: strings.new_builder(1000)
+	}
+
+	if tns.ndims > 2 {
+		mut i := 0
+		for i < (int(tns.ndims / 2) - 1) {
+			offset := tns.ndims - i - 1
+			tmp := prnt.strides[i]
+			prnt.strides[i] = prnt.strides[offset]
+			prnt.strides[offset] = tmp
+
+			tmp2 := prnt.shape[i]
+			prnt.strides[i] = prnt.strides[offset]
+			prnt.strides[offset] = tmp2
+			i++
+		}
+	} else if (tns.ndims == 2) {
+		prnt.strides = prnt.strides.reverse()
+		prnt.shape = prnt.shape.reverse()
+	}
+	prnt.io.write(prnt.obrackets)
+	return prnt
+}
+
+fn (p mut Printer) print() string {
+	val := *(p.iter.next())
+	p.io.write("$val")
+	for {
+		if (!p.inc()) {
+			break
+		}
+	}
+	return p.io.str()
+}
+
+fn (p mut Printer) inc() bool {
+	mut first_item := 0
+	mut ii := 0
+	p.idx[ii] += 1
+	ptr := p.iter.next()
+	for {
+		if (p.idx[ii] != p.shape[ii]) {
+			break
+		}
+		p.idx[ii] = 0
+		ii++
+		if (ii == p.idx.len) {
+			p.io.write("]".repeat(ii))
+			return false
+		}
+		first_item++
+		p.idx[ii] += 1
+	}
+
+	if (ii != 0) {
+		p.io.write("]".repeat(ii))
+		p.io.write(",")
+		p.io.write("\n".repeat(ii))
+		p.io.write(" ".repeat(8))
+		offset := p.t.ndims - ii - 1
+		p.io.write(p.indent[0..offset])
+	}
+
+	if (first_item > 0) {
+		p.io.write("[".repeat(first_item))
+	} else {
+		p.io.write(", ")
+	}
+	val := *ptr
+	p.io.write("$val")
+	return true
+}
+
+pub fn (t Tensor) str() string {
+	mut printer := init_printer(t)
+	return printer.print()
 }
