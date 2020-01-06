42908f0b19c46781671598f2336f885e9f4cedb6
Chris Zimmerman <chris-zimmerman@live.com>
2020-01-06 13:59:20 -0600

    testing fix

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+for module in consts fft internal la ndarray num
+do
+    grep -rl 'import vnum.' ./$module/ | xargs sed -Ei 's/(.*)import vnum\.(.*)/\1import \2/g'
+done
+
+for module in consts fft internal la ndarray num
+do
+    v test $module
+done
+
+for i in consts fft internal la ndarray num
+do
+    for j in consts fft internal la ndarray num
+    do 
+        grep -rl "import $j" ./$i/ | xargs sed -Ei "s/(.*)import $j(.*)/\1import vnum\.$j\2/g" > /dev/null
+    done
+done
