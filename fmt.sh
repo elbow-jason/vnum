366286da19a4594c2889e65e7900fb7bb5b9c29a
Chris Zimmerman <chris-zimmerman@live.com>
2020-01-06 14:23:09 -0600

    Testing new formatter

--- fmt.sh
+++ fmt.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 
-for module in consts fft internal la ndarray num
-do
-    grep -rl 'import vnum.' ./$module/ | xargs sed -Ei 's/(.*)import vnum\.(.*)/\1import \2/g'
-done
+grep -rl 'import vnum.' *.v | xargs sed -Ei 's/(.*)import vnum\.(.*)/\1import \2/g'
+v fmt -w **/*.v
 
-for module in consts fft internal la ndarray num
-do
-    v fmt -w $module/*.v
-done
+
 
 for i in consts fft internal la ndarray num
 do
-    for j in consts fft internal la ndarray num
-    do 
-        grep -rl "import $j" ./$i/ |xargs sed -Ei "s/(.*)import $j(.*)/\1import vnum\.$j\2/g"
-    done
+    grep -rl "import $j" *.v | xargs sed -Ei "s/(.*)import $j(.*)/\1import vnum\.$j\2/g"
 done
--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 for module in consts fft internal la ndarray num
 do
-    grep -rl 'import vnum.' ./$module/ | xargs sed -Ei 's/(.*)import vnum\.(.*)/\1import \2/g'
+    grep -rl 'import vnum.' *.v | xargs sed -Ei 's/(.*)import vnum\.(.*)/\1import \2/g'
 done
 
 for module in consts fft internal la ndarray num
