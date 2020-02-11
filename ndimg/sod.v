f0f51708ceaedc606b8b89b52724f5cc01242424
Chris Zimmerman <chris-zimmerman@live.com>
2020-02-11 16:12:55 -0600

    more img

--- ndimg/sod.v
+++ ndimg/sod.v
@@ -15,6 +15,9 @@ pub mut:
 	data &f64
 }
 
+fn C.sod_free_image(input C.sod_img)
+
+
 fn C.sod_img_load_from_file(z_file byteptr, nchannels int) C.sod_img
 
 
@@ -24,12 +27,31 @@ fn C.sod_img_save_as_png(input C.sod_img, z_path byteptr) int
 fn C.sod_binarize_image(input C.sod_img, reverse int) sod_img
 
 
+fn C.sod_hilditch_thin_image(input sod_img) sod_img
+
+
+fn C.sod_minutiae(input C.sod_img, ptotal &int, pep &int, pbp &int) C.sod_img
+
+
+fn C.sod_canny_edge_image(input C.sod_img, reduce_noise int) C.sod_img
+
+
+fn C.sod_sobel_image(input C.sod_img) C.sod_img
+
+
+fn C.sod_grayscale_image(input C.sod_img) C.sod_img
+
+
 pub fn (img C.sod_img) str() string {
 	return 'Image($img.h, $img.w, $img.c)'
 }
 
+pub fn (img C.sod_img) free() {
+	C.sod_free_image(img)
+}
+
 pub fn (img C.sod_img) to_array() num.NdArray {
-	ret := num.empty([img.h, img.w, img.c])
+	ret := num.empty([img.w, img.h, img.c])
 	mut i := 0
 	for iter := ret.iter(); !iter.done; iter.next() {
 		*iter.ptr = img.data[i]
@@ -39,12 +61,15 @@ pub fn (img C.sod_img) to_array() num.NdArray {
 }
 
 pub fn from_array(arr num.NdArray) C.sod_img {
-	dup := arr.copy('C').buffer()
+	mut downcast := []f32
+	for iter := arr.iter(); !iter.done; iter.next() {
+		downcast << f32(*iter.ptr)
+	}
 	mut img := C.sod_img{}
-	img.h = arr.shape[0]
-	img.w = arr.shape[1]
+	img.w = arr.shape[0]
+	img.h = arr.shape[1]
 	img.c = arr.shape[2]
-	img.data = dup
+	img.data = downcast.data
 	return img
 }
 
@@ -59,4 +84,25 @@ pub fn (img C.sod_img) save_png(fname string) int {
 
 pub fn (img C.sod_img) binarize(reverse int) C.sod_img {
 	return C.sod_binarize_image(img, reverse)
+}
+
+pub fn (img C.sod_img) hilditch() C.sod_img {
+	return C.sod_hilditch_thin_image(img)
+}
+
+pub fn (img C.sod_img) minutiae() C.sod_img {
+	toss := 0
+	return C.sod_minutiae(img, &toss, &toss, &toss)
+}
+
+pub fn (img C.sod_img) canny(reduce_noise int) C.sod_img {
+	return C.sod_canny_edge_image(img, reduce_noise)
+}
+
+pub fn (img C.sod_img) sobel() C.sod_img {
+	return C.sod_sobel_image(img)
+}
+
+pub fn (img C.sod_img) grayscale() C.sod_img {
+	return C.sod_grayscale_image(img)
 }
